601a306472bba65a32b74b80d9aa30e1d1bc6197
dhruvu340 <dhruvu3400@gmail.com>
2024-07-24 16:16:37 -0700

    Update variables.js

--- variables.js
+++ variables.js
@@ -1,5 +1,6 @@
 // video no 1
-
+// there is no need of var in front of identifier but it is a bad practice
+/*multiline comment*/
 // never use var because it can not understand the scope
 
 const accountID=144553
