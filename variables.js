f323b9d83d605bd969b0686f9b373ee21e810072
dhruvu340 <dhruvu3400@gmail.com>
2024-07-24 16:13:29 -0700

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,12 @@
+// video no 1
+
+// never use var because it can not understand the scope
+
+const accountID=144553
+let accoutEMAIL="asc@gmail.com"
+var accountCity="Jaipur"
+accountpass=2345
+accountCity="jaipur"
+// accountID=123
+accoutEMAIL=12445
+console.table([accountCity,accountID,accoutEMAIL,accountpass])
