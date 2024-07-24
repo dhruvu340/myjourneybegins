296ad7f07ca6670cfa5cc916689d3d48f57814bc
dhruvu340 <dhruvu3400@gmail.com>
2024-07-24 16:32:03 -0700

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; //by this use newer version of js
+
+// alert('hello') we are using nodejs not browser
+
+console.log(3 + 4);
+console.log(3);
+
+
+//datatypes
+
+let name444 ="dhruv" //string
+let name2=2//number
+
+
+/*number = 2 to power 53 something
+1 bigint
+2 string=""
+3 boolean->true/false
+4 null is a stand alone value
+5 undefined -> not assign value
+6. symbol use for uniqueness
+
+objects*/
+
+console.log(typeof(undefined))
+console.log(typeof(null))
